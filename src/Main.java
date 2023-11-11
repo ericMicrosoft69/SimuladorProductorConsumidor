ca15b0418dd8ffb264627b9a9a7295b578bf483d
eric <ericMicrosoft69@gmail.com>
2023-11-11 13:32:52 +0100

    problemas al añadir verduras al almacen

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import consumidores.Consumidor;
+import granjeros.Granjero;
 import utils.Verdura;
 
+import static almacen.Almacen.listaVerduras;
+
 public class Main {
-    public static void main(String[] args) {
-        Verdura verdura = new Verdura();
-        System.out.println(verdura.getNombre()+" "+verdura.getTiempoCrecimiento());
+    public static void main(String[] args) throws InterruptedException {
+        int contador = 0;
+        Granjero granjero = new Granjero("Granjero"+contador);
+        granjero.run();
+
 
     }
 }
--- src/almacen/Almacen.java
+++ src/almacen/Almacen.java
@@ -1,4 +1,36 @@
 package almacen;
 
-public class Almacen {
+import utils.Verdura;
+
+import java.util.ArrayList;
+
+public class Almacen extends Verdura {
+    static int capacidad ;
+
+    public static  ArrayList listaVerduras ;
+
+
+    public  static void producir() throws InterruptedException {
+        Verdura verdura = new Verdura();
+        listaVerduras.add(verdura.getNombre());
+        capacidad++;
+        if(capacidad==30){
+            System.out.println("El almacen está lleno");
+        listaVerduras.notifyAll();
+        }
+    }
+
+    public  static void consumir() throws InterruptedException {
+
+            int contador=0;
+            listaVerduras.remove(contador);
+            contador++;
+            capacidad--;
+            if (capacidad==0){
+                System.out.println("El almacen está Vacion");
+                listaVerduras.wait();
+            }
+
+    }
+
 }
