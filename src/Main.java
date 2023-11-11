30be752ae182bf709003e52d2f20746b372f9031
eric <ericMicrosoft69@gmail.com>
2023-11-11 12:29:28 +0100

    Class Verdura

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import utils.Verdura;
 
 public class Main {
     public static void main(String[] args) {
+        Verdura verdura = new Verdura();
+        System.out.println(verdura.getNombre()+" "+verdura.getTiempoCrecimiento());
 
     }
 }
--- src/utils/Verdura.java
+++ src/utils/Verdura.java
@@ -1,4 +1,23 @@
 package utils;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Verdura {
+    public int tiempoCrecimiento;
+    public String nombres[]= {"lettuce", " cabbage", " onion", "spinach", "potato", "celery", "asparagus", "radish", "broccoli", "artichoke", "tomato", "cucumber", " eggplant", " carrot", "green bean"};
+
+    public static int numeroAleatorioEnRango(int minimo, int maximo) {
+        return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
+    }
+
+    public int getTiempoCrecimiento() {
+        int numeroAleatorio= (int) (Math.floor(Math.random() * (10-1+1)) + 1);
+        return numeroAleatorio;
+    }
+
+    public String getNombre() {
+        int posicionAleatoria = numeroAleatorioEnRango(0, nombres.length - 1);
+        return nombres[posicionAleatoria];
+    }
 }
